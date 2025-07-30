98bec252cc38bd1eeb1f9e2969f3a06b3c5a331d
Rosen <luosensen725@163.com>
2025-07-30 10:29:18 +0800

    初版

---
+++ UI/src/ui/originUI.java
@@ -0,0 +1,11 @@
+package ui;
+
+import javax.swing.*;
+
+public class originUI {
+    public static void main(String[] args) {
+        JFrame gamejframe =new JFrame("拼图游戏");
+        gamejframe.setSize(600,680);
+        gamejframe.setVisible(true);
+    }
+}
